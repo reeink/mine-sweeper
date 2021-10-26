875d7e0c54aaaaad47b63bc2ce1d3ec12416e281
sperjar <sperjarg@gmail.com>
2021-10-26 23:07:46 +0800

    scene basic

--- include/scene.h
+++ include/scene.h
@@ -1,16 +1,52 @@
-#progma once
+#pragma once
+#include <QObject>
+#include <QMouseEvent>
+#include <QGraphicsScene>
+#include <QPainter>
+#include <QWidget>
 
-class Block
+class Block : public QObject
 {
+    Q_OBJECT;
 
+protected:
+    int row_;
+    int col_;
+
+signals:
+    void click(const int &row, const int &col);
+    void mark(const int &row, const int &col);
+    void check();
+
+public:
+    void mousePressEvent(QMouseEvent *event);
+    void init(const int &row, const int &col);
 };
 
 class BlockPainter : public Block
 {
+    Q_OBJECT;
+    friend class Scene;
 
+private:
+    QPainter *painter_;
+
+public slots:
+    void paint();
+
+public:
+    BlockPainter(QPainter *&painter);
 };
 
-class Scene
+class Scene : public QWidget
 {
+    Q_OBJECT;
 
+private:
+    QPainter *painter_;
+    BlockPainter *block_;
+
+public:
+    Scene(const int &row, const int &col);
+    ~Scene();
 };
--- src/scene.c
+++ src/scene.c
@@ -1,1 +1,53 @@
 #include "../include/scene.h"
+
+// class Block
+void Block::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        emit click(row_, col_); //点击方块信号
+    }
+    else if (event->button() == Qt::RightButton)
+    {
+        emit mark(row_, col_); //标记方块信号
+    }
+
+    emit check(); //检查是否做完
+}
+
+void Block::init(const int &row, const int &col)
+{
+    row_ = row;
+    col_ = col;
+}
+
+// class BlockPainter
+void BlockPainter::paint()
+{
+}
+
+BlockPainter::BlockPainter(QPainter *&painter) : painter_(painter)
+{
+}
+
+// class Scene
+Scene ::Scene(const int &row, const int &col)
+{
+    painter_ = new QPainter(this);
+    block_ = new BlockPainter[row * col](painter_);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            block_[i * col + j].init(i, j);
+            // TODO: connect!
+        }
+    }
+}
+
+Scene::~Scene()
+{
+    delete[] painter_;
+    delete[] block_;
+}
